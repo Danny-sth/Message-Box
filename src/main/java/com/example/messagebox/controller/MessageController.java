11d40d5d7c3ecc896977aaa578fa19c8e5d54982
Denis <danny.wasth@gmail.com>
2021-09-16 14:28:20 +0300

    Реализовал методы CRUD-операций над сообщениями

--- src/main/java/com/example/messagebox/controller/MessageController.java
+++ src/main/java/com/example/messagebox/controller/MessageController.java
@@ -1,10 +1,7 @@
 package com.example.messagebox.controller;
 
 import com.example.messagebox.exceptions.NotFoundException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -15,7 +12,8 @@ import java.util.Map;
 @RequestMapping("/messages")
 public class MessageController {
 
-    public List<Map<String, String>> messageList = new ArrayList<>(){{
+    private int counter = 4;
+    private List<Map<String, String>> messageList = new ArrayList<>(){{
         add(new HashMap<String, String>() {{ put("id", "1"); put("text", "First message"); }});
         add(new HashMap<String, String>() {{ put("id", "2"); put("text", "Second message"); }});
         add(new HashMap<String, String>() {{ put("id", "3"); put("text", "Third message"); }});
@@ -27,15 +25,38 @@ public class MessageController {
     }
 
     @GetMapping("/{id}")
-    public Map<String, String> getMessageByID(@PathVariable int id) {
+    public Map<String, String> getMessageByID(@PathVariable String id) {
         return getByIdFromMessageList(id);
     }
 
-    private Map<String, String> getByIdFromMessageList(int id) {
+    private Map<String, String> getByIdFromMessageList(String id) {
         return messageList.stream()
                 .filter(message -> message.get("id").equals(id))
                 .findFirst()
                 .orElseThrow(NotFoundException::new);
+    }
+
+    @PostMapping
+    public Map<String, String> createMessage(
+            @RequestBody Map<String, String> message) {
+        message.put("id", String.valueOf(counter++));
+        messageList.add(message);
+        return message;
+    }
+
+    @PutMapping("{id}")
+    public Map<String, String> editMessage(@PathVariable String id,
+                                           @RequestBody Map<String, String> message) {
+        Map<String, String> messageForEdit = getByIdFromMessageList(message.get("id"));
+        messageForEdit.putAll(message);
+        messageForEdit.put("id", id);
+        return messageForEdit;
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteMessage(@PathVariable String id) {
+        Map<String, String> message = getByIdFromMessageList(id);
+        messageList.remove(message);
     }
 
 }
